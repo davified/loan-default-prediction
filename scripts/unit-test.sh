4027459efe8dd59b2e4d5a71bf8dc54f344ee2f9
David Tan <davidtan@thoughtworks.com>
2022-09-30 09:48:55 +1000

    Simplify unit-test.sh

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -2,14 +2,4 @@
 
 set -e
 
-echo "Creating virtual environment..."
-python -m venv /code/.venv-dev
-
-echo "Activating virtual environment..."
-. /code/.venv-dev/bin/activate
-
-echo "Installing python dev dependencies..."
-pip install --upgrade pip
-pip install -r requirements-dev.txt
-
 python -m unittest discover ./tests/
