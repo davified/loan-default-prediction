c9792139c875c1c2ec6c90c6019f57e1ab1f83c5
David Tan <davidtan@thoughtworks.com>
2022-10-21 10:46:07 +1100

    Add go scripts for Mac, Linux and Windows

--- scripts/go/go-mac.sh
+++ scripts/go/go-mac.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+set -e
+
 echo "Installing homebrew if it's not installed..."
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "Installing docker if it's not installed..."
-brew install  --cask docker
+which docker || brew install --cask docker
 
-# python dependencies, needed for configuring IDE to have proper intellisense and autocomplete
-echo "Installing python3 if it's not installed..."
-which python3 || brew install python3
+echo "Installing colima (an open-source license-free container runtime)"
+which colima || brew install colima docker-credential-helper
