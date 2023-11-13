85c9451c08b3ae9ca2c4f20e662ae7035e34b22b
David Tan <davidtan@thoughtworks.com>
2023-11-13 17:01:17 +1100

    Remove git hooks

--- scripts/go/go-mac.sh
+++ scripts/go/go-mac.sh
@@ -11,11 +11,5 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 echo "Installing Python 3 if it's not installed..."
 which python3 || brew install python3
 
-echo "Installing lefthook if it's not installed..."
-which lefthook || brew install lefthook
-
-echo "Setting up git hooks for this project"
-lefthook install
-
 echo "Installing dependencies on host (so that we can configure a virtual environment for our IDE)"
 ./scripts/go/install-dependencies-on-host.sh
--- scripts/go/go-windows.bat
+++ scripts/go/go-windows.bat
@@ -9,11 +9,5 @@ poetry config virtualenvs.in-project false
 ECHO "Installing dependencies..."
 poetry install
 
-where lefthook
-IF %ERRORLEVEL% EQU 0 (ECHO "lefthook is installed") ELSE (winget install evilmartians.lefthook)
-
-ECHO "Setting up git hooks for this project"
-lefthook install
-
 ECHO "Done.  Configure your IDE to use the .\Scripts\python.exe interpreter in the following virtual environment path"
 poetry env info -p
